c8cbaedc0b2315721183283cd80972ad2f2f1148
= <alex.a4.25@yandex.ru>
2020-12-18 10:59:58 +0300

    Init position page

--- lib/app/navigation/router.dart
+++ lib/app/navigation/router.dart
@@ -4,6 +4,7 @@ import 'package:multi_platform_course/app/pages/employee_page/employee_fitter_pa
 import 'package:multi_platform_course/app/pages/employee_page/employee_view_page.dart';
 import 'package:multi_platform_course/app/pages/main_page/main_page.dart';
 import 'package:multi_platform_course/app/pages/position_page/position_add_page.dart';
+import 'package:multi_platform_course/app/pages/position_page/position_view_page.dart';
 import 'package:multi_platform_course/data/core/service_locator.dart';
 
 const ROOT = '/';
@@ -11,6 +12,7 @@ const EMPLOYEES_VIEW = '/employees';
 const EMPLOYEES_VIEW_FILTER = '/employees_filter';
 const EMPLOYEE_ADD = '/employee/add';
 const POSITION_VIEW = '/positions';
+const POSITION_VIEW_OPEN = '/positions_open';
 const POSITION_ADD = '/position/add';
 
 Map<String, WidgetBuilder> routes = {
@@ -18,7 +20,8 @@ Map<String, WidgetBuilder> routes = {
   EMPLOYEES_VIEW: (_) => EmployeePage(bloc: sl()),
   EMPLOYEES_VIEW_FILTER: (_) => EmployeeFilterPage(bloc: sl()),
   EMPLOYEE_ADD: (_) => EmployeeAddPage(repository: sl()),
-  POSITION_VIEW: (_) => null,
+  POSITION_VIEW: (_) => PositionPage(bloc: sl()),
+  POSITION_VIEW_OPEN: (_) => null,
   POSITION_ADD: (_) => PositionAddPage(repository: sl()),
 };
 
